1adcb225a30fe65758cb21f790139257ff1fd124
Shogo Ogami <sg.tao.so@gmail.com>
2020-05-20 08:04:34 +0900

    update iframePage to exchange blank page on public server.

--- 03_projects/10_iframePage/js/main.js
+++ 03_projects/10_iframePage/js/main.js
@@ -2,40 +2,40 @@
 
 {
   const menu_contents = [
-    { id: 141003, menu: "横浜市", type: "ordinance-city", url: "https://www.city.yokohama.lg.jp/city-info/koho-kocho/press/", load: "success"},
-    { id: 141305, menu: "川崎市", type: "ordinance-city", url: "http://www.city.kawasaki.jp/", load: "success"},
+    { id: 141003, menu: "横浜市", type: "ordinance-city", url: "https://www.city.yokohama.lg.jp/city-info/koho-kocho/press/", load: "success" },
+    { id: 141305, menu: "川崎市", type: "ordinance-city", url: "http://www.city.kawasaki.jp/", load: "maybe" },
     { id: 141500, menu: "相模原市", type: "ordinance-city", url: "https://www.city.sagamihara.kanagawa.jp/shisei/koho/houdo/index.html", load: "refused" },
-    { id: 14201, menu: "横須賀市", type: "general-city", url: "https://www.city.yokosuka.kanagawa.jp/shisei/shinchaku/index.html" },
-    { id: 14203, menu: "平塚市", type: "general-city", url: "http://www.city.hiratsuka.kanagawa.jp/news_category6.html" },
-    { id: 14204, menu: "鎌倉市", type: "general-city", url: "https://www.city.kamakura.kanagawa.jp/oshirase/index.html" },
-    { id: 14205, menu: "藤沢市", type: "general-city", url: "https://www.city.fujisawa.kanagawa.jp/shinchaku/index.html" },
-    { id: 14206, menu: "小田原市", type: "general-city", url: "http://www.city.odawara.kanagawa.jp/topix/" },
+    { id: 14201, menu: "横須賀市", type: "general-city", url: "https://www.city.yokosuka.kanagawa.jp/shisei/shinchaku/index.html", load: "success" },
+    { id: 14203, menu: "平塚市", type: "general-city", url: "http://www.city.hiratsuka.kanagawa.jp/news_category6.html", load: "maybe" },
+    { id: 14204, menu: "鎌倉市", type: "general-city", url: "https://www.city.kamakura.kanagawa.jp/oshirase/index.html", load: "success" },
+    { id: 14205, menu: "藤沢市", type: "general-city", url: "https://www.city.fujisawa.kanagawa.jp/shinchaku/index.html", load: "success" },
+    { id: 14206, menu: "小田原市", type: "general-city", url: "http://www.city.odawara.kanagawa.jp/topix/", load: "maybe" },
     { id: 14207, menu: "茅ヶ崎市", type: "general-city", url: "https://www.city.chigasaki.kanagawa.jp/about/update.html", load: "refused" },
-    { id: 14208, menu: "逗子市", type: "general-city", url: "https://www.city.zushi.kanagawa.jp/topics/" },
-    { id: 14210, menu: "三浦市", type: "general-city", url: "http://www.city.miura.kanagawa.jp/shisei-whatsnew.html" },
+    { id: 14208, menu: "逗子市", type: "general-city", url: "https://www.city.zushi.kanagawa.jp/topics/", load: "success" },
+    { id: 14210, menu: "三浦市", type: "general-city", url: "http://www.city.miura.kanagawa.jp/shisei-whatsnew.html", load: "maybe" },
     { id: 14211, menu: "秦野市", type: "general-city", url: "https://www.city.hadano.kanagawa.jp/www/news.html", load: "refused" },
-    { id: 14212, menu: "厚木市", type: "general-city", url: "https://www.city.atsugi.kanagawa.jp/information/news.html" },
-    { id: 14213, menu: "大和市", type: "general-city", url: "http://www.city.yamato.lg.jp/web/news/newslist.html" },
-    { id: 14214, menu: "伊勢原市", type: "general-city", url: "https://www.city.isehara.kanagawa.jp/categories/bunya/shisei/more@docs_3@c_shinchaku.html" },
+    { id: 14212, menu: "厚木市", type: "general-city", url: "https://www.city.atsugi.kanagawa.jp/information/news.html", load: "success" },
+    { id: 14213, menu: "大和市", type: "general-city", url: "http://www.city.yamato.lg.jp/web/news/newslist.html", load: "maybe" },
+    { id: 14214, menu: "伊勢原市", type: "general-city", url: "https://www.city.isehara.kanagawa.jp/categories/bunya/shisei/more@docs_3@c_shinchaku.html", load: "success" },
     { id: 14215, menu: "海老名市", type: "general-city", url: "https://www.city.ebina.kanagawa.jp/newslist.html", load: "refused" },
-    { id: 14216, menu: "座間市", type: "general-city", url: "https://www.city.zama.kanagawa.jp/www/topics/0000000000000/ACL04000.html" },
-    { id: 14217, menu: "南足柄市", type: "general-city", url: "http://www.city.minamiashigara.kanagawa.jp/newsTopics/" },
-    { id: 14218, menu: "綾瀬市", type: "general-city", url: "https://www.city.ayase.kanagawa.jp/index.html#tab1" },
-    { id: 14301, menu: "三浦郡葉山町", type: "town", url: "https://www.town.hayama.lg.jp/new.html" },
+    { id: 14216, menu: "座間市", type: "general-city", url: "https://www.city.zama.kanagawa.jp/www/topics/0000000000000/ACL04000.html", load: "success" },
+    { id: 14217, menu: "南足柄市", type: "general-city", url: "http://www.city.minamiashigara.kanagawa.jp/newsTopics/", load: "maybe" },
+    { id: 14218, menu: "綾瀬市", type: "general-city", url: "https://www.city.ayase.kanagawa.jp/index.html#tab1", load: "success" },
+    { id: 14301, menu: "三浦郡葉山町", type: "town", url: "https://www.town.hayama.lg.jp/new.html", load: "success" },
     { id: 14321, menu: "高座郡寒川町", type: "town", url: "http://www.town.samukawa.kanagawa.jp/oshirase/index.html" },
-    { id: 14341, menu: "中郡大磯町", type: "town", url: "http://www.town.oiso.kanagawa.jp/news.html" },
-    { id: 14342, menu: "中郡二宮町", type: "town", url: "http://www.town.ninomiya.kanagawa.jp/news.html" },
-    { id: 14361, menu: "足柄上郡中井町", type: "town", url: "https://www.town.nakai.kanagawa.jp/local//common/controls/alllist/AllList.aspx?type=new" },
-    { id: 14362, menu: "足柄上郡大井町", type: "town", url: "https://www.town.oi.kanagawa.jp/soshiki/list1-1.html" },
-    { id: 14363, menu: "足柄上郡松田町", type: "town", url: "https://town.matsuda.kanagawa.jp/soshiki/list1-1.html" },
-    { id: 14364, menu: "足柄上郡山北町", type: "town", url: "http://www.town.yamakita.kanagawa.jp/news/0001.html" },
+    { id: 14341, menu: "中郡大磯町", type: "town", url: "http://www.town.oiso.kanagawa.jp/news.html", load: "maybe" },
+    { id: 14342, menu: "中郡二宮町", type: "town", url: "http://www.town.ninomiya.kanagawa.jp/news.html", load: "maybe" },
+    { id: 14361, menu: "足柄上郡中井町", type: "town", url: "https://www.town.nakai.kanagawa.jp/local//common/controls/alllist/AllList.aspx?type=new", load: "success" },
+    { id: 14362, menu: "足柄上郡大井町", type: "town", url: "https://www.town.oi.kanagawa.jp/soshiki/list1-1.html", load: "success" },
+    { id: 14363, menu: "足柄上郡松田町", type: "town", url: "https://town.matsuda.kanagawa.jp/soshiki/list1-1.html", load: "success" },
+    { id: 14364, menu: "足柄上郡山北町", type: "town", url: "http://www.town.yamakita.kanagawa.jp/news/0001.html", load: "maybe" },
     { id: 14366, menu: "足柄上郡開成町", type: "town", url: "https://www.town.kaisei.kanagawa.jp/forms/top/top.aspx#tab2", load: "refused" },
-    { id: 14382, menu: "足柄下郡箱根町", type: "town", url: "http://www.town.hakone.kanagawa.jp/" },
-    { id: 14383, menu: "足柄下郡真鶴町", type: "town", url: "http://www.town.manazuru.kanagawa.jp/news.html" },
-    { id: 14384, menu: "足柄下郡湯河原町", type: "town", url: "https://www.town.yugawara.kanagawa.jp/information/" },
-    { id: 14401, menu: "愛甲郡愛川町", type: "town", url: "https://www.town.aikawa.kanagawa.jp/news_list.html" },
-    { id: 14402, menu: "愛甲郡清川村", type: "village", url: "https://www.town.kiyokawa.kanagawa.jp/news.html" },
-    { id:999, menu:"dummy" , type: "dummy", url:"dummy" }
+    { id: 14382, menu: "足柄下郡箱根町", type: "town", url: "http://www.town.hakone.kanagawa.jp/", load: "maybe" },
+    { id: 14383, menu: "足柄下郡真鶴町", type: "town", url: "http://www.town.manazuru.kanagawa.jp/news.html", load: "maybe" },
+    { id: 14384, menu: "足柄下郡湯河原町", type: "town", url: "https://www.town.yugawara.kanagawa.jp/information/", load: "success" },
+    { id: 14401, menu: "愛甲郡愛川町", type: "town", url: "https://www.town.aikawa.kanagawa.jp/news_list.html", load: "success" },
+    { id: 14402, menu: "愛甲郡清川村", type: "village", url: "https://www.town.kiyokawa.kanagawa.jp/news.html", load: "success" },
+    { id: 999, menu: "dummy", type: "dummy", url: "dummy", load: "dummy" }
   ]; 
   console.dir(menu_contents);
 
@@ -46,6 +46,14 @@
   var tab_menus = document.getElementById('tab_menus');
   var tab_contents = document.getElementById('tab_contents');
 
+  // check site domain
+  var site_domain = document.domain;
+  var private_server = (site_domain != "github.io") ? true : false;
+  console.log('private_server : ' + private_server);
+  function blankpage_str(url) {
+    return `<a href="${url}" target="_blank">こちら</a>のリンクより開きます（別ウィンドウ）。`;
+  }
+    
   // check select area
   const chkbox_pref = document.querySelector("div#select-area input[name='pref']");
   const chkbox_ordinance = document.querySelector("div#select-area input[name='ordinance']");
@@ -83,8 +91,15 @@
   window.onload = function () {
     var menus = menu.getElementsByTagName('a');
     let i;
-    console.log("menus(basic)" + menus);
+    console.dir(menus);
     tab_init(menus[0]);
+    // at prefecture page, change innerHTML on public_server
+    if (private_server == false) { // on public_server
+      let page = document.querySelector("div#page-0");
+      let page_iframe = page.querySelector("iframe");
+      console.log("on public_server, pref. page is changed to blank page");
+      page.innerHTML = blankpage_str(page_iframe.src);
+    }
 
     menu_contents.forEach(function (key){
       console.dir(key);
@@ -137,11 +152,11 @@
     add_page.id = "page-" + key.id;
     add_page.className = "page";
     add_page.style.display = 'none';
-    if ((key.load == null) || (key.load === "success")) {
+    if ((key.load == "success") || (key.load == "maybe" && private_server == true)) {
       add_iframe.setAttribute("src", key.url);
       add_page.appendChild(add_iframe);
-    } else { // load is "refused"
-      add_parag.innerHTML = "<a href=\"" + key.url + "\" target=\"_blank\">こちら</a>のリンクより開きます（別ウィンドウ）。"
+    } else { // load is "refused" or "maybe" on public_server
+      add_parag.innerHTML = blankpage_str(key.url);
       add_page.appendChild(add_parag);
     }
     target_contents.appendChild(add_page);
