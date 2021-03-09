9e47b214ab59d5f2c2e20940108e0185fa2f14d0
Shogo Ogami <sg.tao.so@gmail.com>
2021-03-10 08:02:56 +0900

    rebuild mysite by refer to CodeMafia's lessons

---
+++ scripts/libs/hero-slider.js
@@ -0,0 +1,50 @@
+class HeroSlider {
+    constructor(el) {
+        this.el = el;
+        this.swiper = this._initSwiper();
+    }
+
+    _initSwiper() {
+        return new Swiper(this.el, {
+            // Optional parameters
+            // direction: 'vertical',
+            loop: true,
+            grabCursor: true,
+            effect: 'coverflow',
+            centeredSlides: true,
+            slidesPerView: 1,
+            speed: 1000,
+            breakpoints: {
+                1024: {
+                    slidesPerView: 2,
+                }
+            },
+            autoplay: {
+                delay: 2500,
+                disableOnInteraction: false,
+            },
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+
+        });
+    }
+
+    start(options = {}) {
+        options = Object.assign({
+            delay: 4000,
+            disableOnInteraction: false
+        }, options);
+        
+        this.swiper.params.autoplay = options;
+        this.swiper.autoplay.start();
+    }
+    stop() {
+        this.swiper.autoplay.stop();
+    }
+}
