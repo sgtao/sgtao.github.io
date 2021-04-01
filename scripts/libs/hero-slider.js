709dad6aeadbacee8734430ba88981705d130ee8
Shogo Ogami <sg.tao.so@gmail.com>
2021-04-02 07:45:37 +0900

    update hero-slider for add number pagination

--- scripts/libs/hero-slider.js
+++ scripts/libs/hero-slider.js
@@ -26,6 +26,9 @@ class HeroSlider {
             pagination: {
                 el: '.swiper-pagination',
                 clickable: true,
+                renderBullet: function (index, className) {
+                    return '<span class="' + className + '">' + (index + 1) + '</span>';
+                },
             },
             navigation: {
                 nextEl: '.swiper-button-next',
